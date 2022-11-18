006ac513d982832070de9754dcd15052e8744f0f
Ujjval Patel <upatel69@myseneca.ca>
2022-11-18 13:22:23 -0500

    get All emps done

--- data-service.js
+++ data-service.js
@@ -1,28 +1,74 @@
-// const fileSystem = require('fs'); // including for reading files
-// const { resolve } = require('path');
-// let employees = []; // to hold the employees in the array
-// let departments = []; // to hold departments in array 
-// let employee = [] // to hold results temporarily
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize('wlpgyefj', 'wlpgyefj', 'mtVk-HK7nhXwcAiPwf_nYNhr09vsHqyH', {
+  host: 'peanut.db.elephantsql.com',
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+  ssl: true
+  },
+  query:{raw: true}
+});
+
+sequelize
+.authenticate()
+.then(()=> console.log('Connection success.'))
+.catch((err)=>console.log("Unable to connect to DB.", err));
+
+let Employee = sequelize.define('Employee', {
+  employeeNum:{
+    type:Sequelize.INTEGER,
+    primaryKey: true, 
+    autoIncrement: true
+  },
+  firstName:Sequelize.STRING,
+  lastName:Sequelize.STRING,
+  email:Sequelize.STRING,
+  SSN:Sequelize.STRING,
+  addressStreet:Sequelize.STRING,
+  addressCity:Sequelize.STRING,
+  addressState:Sequelize.STRING,
+  addressPostal:Sequelize.STRING,
+  maritalStatus:Sequelize.STRING,
+  isManager:Sequelize.BOOLEAN,
+  employeeManagerNum:Sequelize.INTEGER,
+  status:Sequelize.STRING,
+  department:Sequelize.INTEGER,
+  hireDate:Sequelize.STRING
+});
+
+let Department = sequelize.define('Department',{
+  departmentId:{
+    type:Sequelize.INTEGER,
+    primaryKey: true, 
+    autoIncrement: true
+  },
+  departmentName:Sequelize.STRING
+});
+
 
 module.exports.initialize = function () {
   return new Promise((resolve, reject) => {
-   reject();
+    sequelize.sync()
+    .then(()=>resolve())
+    .catch(()=>reject("Unable to synchronize the database"));
   });
 };
 
 
 module.exports.getAllEmployees = function () {
   return new Promise((resolve, reject) => {
-    reject();
+    Employee.findAll()
+    .then((data)=>resolve(data))
+    .catch(()=> reject("Error! No Employees returned..!!"));
   });
 };
   
 
-module.exports.getManagers = function () {
-  return new Promise((resolve, reject) => {
-    reject();
-  });
-};
+// module.exports.getManagers = function () {
+//   return new Promise((resolve, reject) => {
+//     reject();
+//   });
+// };
   
   
 module.exports.getDepartments = function () {
