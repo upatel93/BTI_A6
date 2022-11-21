099582d54e3c7f81cf54fcf6e895ecac1050bfb1
Ujjval Patel <upatel69@myseneca.ca>
2022-11-21 13:51:14 -0500

    need to correct the functions

--- data-service.js
+++ data-service.js
@@ -73,45 +73,88 @@ module.exports.getAllEmployees = function () {
   
 module.exports.getDepartments = function () {
   return new Promise((resolve, reject) => {
-    reject();
+    Department.findAll()
+    .then((data)=>resolve(data))
+    .catch(()=>reject("Error! No Results for Departments"));
   });
 };
 
 
 module.exports.addEmployee = (employeeData)=>{
   return new Promise((resolve,reject)=>{
-    reject();
+    
+    //1. correcting isManager
+    employeeData.isManager = employeeData.isManager == undefined ? false:employeeData.isManager
+
+    //2. correcting key:"" to key:null
+    for(let i in employeeData){
+      if(i === ""){
+        i = null;
+      }
+    }
+
+    Employee.create(employeeData)
+    .then(()=>resolve())
+    .catch(()=>reject("Error! Unable to create Employee..!!"));
+
   });
 };
 
 module.exports.updateEmployee = (employeeData)=>{
   return new Promise((resolve,reject)=>{
-    reject();
+    //1. correcting isManager
+    employeeData.isManager = employeeData.isManager == undefined ? false:employeeData.isManager
+
+    //2. correcting key:"" to key:null
+    for(let i in employeeData){
+      if(i === ""){
+        i = null;
+      }
+    }
+
+    Employee.update(employeeData,{where:{SSN:employeeData.SSN}})
+    .then(()=>resolve())
+    .catch(()=>reject("Error! Unable to update employee..!!"));
   })
 }
 
-module.exports.getEmployeesByStatus = (status)=>{
+module.exports.getEmployeesByStatus = (Status)=>{
   return new Promise((resolve,reject)=>{
-    reject();
+    Employee.findAll({status:Status})
+    .then((data)=>resolve(data))
+    .catch(()=>reject("No Result returned for Employee's status"));    
   });
 };
 
 
-module.exports.getEmployeesByDepartment = (department)=>{
+module.exports.getEmployeesByDepartment = (Department)=>{
   return new Promise((resolve,reject)=>{
-    reject();
+      Employee.findAll({department:Department})
+      .then((data)=>resolve(data))
+      .catch(()=>reject("No Result returned for Department"));
   });
 };
 
 
-module.exports.getEmployeesByManager = (manager)=>{
+module.exports.getEmployeesByManager = (Manager)=>{
   return new Promise((resolve,reject)=>{
-    reject();
+    Employee.findAll({
+      where:{employeeManagerNum:Manager.value}
+    })
+      .then((data)=>resolve(data))
+      .catch(()=>reject("No Result returned for Manager"));
   });
 };
 
 module.exports.getEmployeeByNum = (num)=>{
   return new Promise((resolve,reject)=>{
-    reject();
+    console.log(num);
+    Employee.findAll({
+    where: {
+      employeeNum:num.value
+    }})
+      .then((data)=>{resolve(data)
+      console.log(data)})
+      .catch(()=>reject("No Result returned for Employee_Num"));
   });
 };
